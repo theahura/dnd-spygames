df6927910adbc74f12183f15a3947d80a801f050
Amol Kapoor <theahura@gmail.com>
2017-07-01 17:05:45 -0400

    Got chat working, added dnd dice roll parser.

--- js/main.js
+++ js/main.js
@@ -9,8 +9,59 @@ var username = window.prompt('What is your name?');
 
 socket.emit('setName', {'name':username});
 
-$('.attack-form-holder').submit(function(){
+var canSubmitAttack = true;
+
+function process_attack_data() {
+	speed = droll.roll($('#speed').val())
+	accuracy = droll.roll($('#accuracy').val())
+	damage = droll.roll($('#damage').val())
+
+	alert(speed)
+	alert(accuracy)
+	alert(damage)
+
+	if (speed && accuracy && damage)
+		return {
+			sender: username,
+			speed: speed.total,
+			accuracy: accuracy.total,
+			damage: damage.total,
+			other: $('#other').val()
+		};
+	return false;
+}
+
+$('.attack-form-submit').click(function(){
+	if (!canSubmitAttack) {
+		alert('Cant submit attack yet!');
+		return;
+	}
+
+	data = process_attack_data();
+	if (!data) {
+		alert('Error in attack form!');
+		return;
+	}
+
+	socket.emit('charInputAttack', data);
+	console.log(data)
+
+	canSubmitAttack = false;
 });
 
-$('.chat-input').submit(function(){
+$('.chat-input-textarea').keypress(function (e) {
+	if (e.which == 13) {
+		data = {
+			sender: username,
+			message: $(this).val()
+		}
+		socket.emit('chatGroup', data)
+		$(this).val('')
+	}
 });
+
+socket.on('chat', function(data) {
+	$('.chat-text').append($('<li>').text(data.sender + ': ' + data.message));
+});
+
+
--- server.js
+++ server.js
@@ -18,28 +18,42 @@ var encounterPlayers = 0;
 
 function orderAttacksAndSendToDM(attacks, dmSocket) {
 	attacks.sort(function(a, b) {
-		if (a.initiative < b.initiative)
+		if (a.speed < b.speed)
 			return -1;
-		else if (a.initiative > b.initiative)
+		else if (a.speed > b.speed)
 			return 1;
 		else
 			return 0;
 	});
 
-	io.to(dm.id).emit('setMastermind', attacks);
+	io.to(dm.id).emit('sortedAttacks', attacks);
 	attacks = []
 }
 
 io.sockets.on('connection', function(socket) {
 
+	var playerName = '':
+
 	socket.on('disconnect', function() {
      	console.log('Got disconnect!');
+		data = {
+			sender: 'server',
+			message: playerName + ' left the chat.' 
+		}
+		io.sockets.emit('chat', data);
+		delete players[playerName];
    	});
 
 	socket.on('setName', function(data, callback) {
 		if (data.name === 'dm')
 			dm = socket;
 		players[data.name] = socket;
+		playerName = data.name;
+		chatData = {
+			sender:'server',
+			message: data.name + ' joined the chat.'
+		}
+		io.sockets.emit('chat', chatData);
 	});
 
 	socket.on('dmSetNumPlayersEncounter', function(data, callback) {
@@ -60,7 +74,6 @@ io.sockets.on('connection', function(socket) {
 		}
 
 		io.to(players[data.sendto].id).emit('setMastermind', mastermind_data)
-
 	});
 
 	socket.on('charInputAttack', function(data, callback) {
